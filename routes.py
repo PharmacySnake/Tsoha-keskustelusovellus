0c6fcbfbe18722f5757ca5c7ab7031c6f92b918f
Andreas Katsaras <andreas.katsaras@gmail.com>
2022-01-27 06:28:54 +0200

    tried to make fixes to run pages without errors

---
+++ routes.py
@@ -0,0 +1,76 @@
+from app import app
+from flask import render_template, redirect, request, session
+
+import users
+import messages
+
+
+@app.route("/")
+def index():
+    list = messages.get_list()
+    return render_template("index.html", count=len(list), messages=list)
+
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    if messages.send(content):
+        return redirect("/")
+    else:
+        return render_template("errorrr.html", message="Viestin lähetys ei onnistunut")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            #session["username"] = username
+            return redirect("/")
+        else:
+            return render_template("errorrr.html", message="Väärä tunnus tai salasana")
+
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("errorrr.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            #users.login(username, password1)
+            return redirect("/")
+        else:
+            return render_template("errorrr.html", message="Rekisteröinti ei onnistunut")
+
+'''     
+@app.route("/account")
+def account():
+    return render_template("register.html")
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 == password2:
+        hash_value = generate_password_hash(password1)
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+    return render_template("index.html")
+'''
