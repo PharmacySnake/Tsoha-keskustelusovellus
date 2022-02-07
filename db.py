fd303bdc2ab0633a1e16a9ff3ad2cbbd0b8f8743
Andreas Katsaras <andreas.katsaras@gmail.com>
2022-02-07 06:11:54 +0200

    more and more tries to fix heroku

--- db.py
+++ db.py
@@ -4,5 +4,9 @@ from os import getenv
 
 #app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = app.config["SQLALCHEMY_DATABASE_URI"].replace(
+        "postgres://", "postgresql://", 1)
+    
 db = SQLAlchemy(app)
 
